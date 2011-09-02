2a9d973eda8c1a62b54faceaaf1ad3c7685d5c20
James Van Dyke <jamesvd@gmail.com>
2011-09-02 14:17:20 -0400

    Improved formatting for results. Fixed package.json to reflect eventual project name change. Added jsdom functionality, although untesed. Modfied bin/jasmine-node to run in modern versions of nodejs.

--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -1,5 +1,11 @@
 describe('jasmine-node-flat', function(){
-  it('should pass', function(){
+  it('should print from parent suite', function(){
     expect(1+2).toEqual(3);
+  });
+  
+  describe("Nested", function() {
+  	it("should print from child suite", function() {
+  		expect(2).toEqual(2);
+  	});
   });
 });
